ee3c81eb6a5fab201df7bb6d0b2827d4d56ab25b
George Pittarelli <tf2stadium@gjp.cc>
2016-11-01 22:31:50 -0700

    upgrade to SM v1.8

--- download-sourcemod.sh
+++ download-sourcemod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-LATESTSM=$(wget -o /dev/null -O - https://www.sourcemod.net/smdrop/1.7/sourcemod-latest-linux)
-wget https://www.sourcemod.net/smdrop/1.7/$LATESTSM
+LATESTSM=$(wget -o /dev/null -O - https://www.sourcemod.net/smdrop/1.8/sourcemod-latest-linux)
+wget https://www.sourcemod.net/smdrop/1.8/$LATESTSM
 
 tar xzvf $LATESTSM addons/sourcemod --strip-components=1
 
